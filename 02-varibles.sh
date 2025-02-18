d861ed8b2a00aa2fac1de82222fe31d6536774fb
charan <medasanicharan66@gmail.com>
2025-02-18 21:55:22 +0530

    shell-script

--- 02-varibles.sh
+++ 02-varibles.sh
@@ -1,1 +1,6 @@
-this is samle script
+#!/bin/bash
+
+echo "ramesh:: hello suresh, how are you?"
+echo "suresh:: hi ramesh, i am fine. how are you"
+echo "ramesh:: i am fine too. how is your work"
+echo "suresh:: not bad. i am thinking to upgrade to devops"`
