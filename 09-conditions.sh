baaa148ca9e7a8eff1e1d6cae3583c3e763bd40d
charan <medasanicharan66@gmail.com>
2025-02-19 10:59:34 +0530

    shell-script

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -8,3 +8,12 @@ then
 else
     echo "given number $NUMBER is less than 10"
 fi
+
+# -gt(greaterthan), -lt(lessthan), -eq(equal), -ge(greaterthan equal to), -le(lessthan equal to)
+
+if [ $NUMBER -lt 20 ]
+then
+    echo "given number $NUMBER is greater than 20"
+else
+    echo "given number $NUMBER is less than 20"
+fi
