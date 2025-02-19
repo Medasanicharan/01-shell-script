49172fe285f45713fdb68b8636f803910fab49e3
charan <medasanicharan66@gmail.com>
2025-02-19 15:36:06 +0530

    shell-script

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -2,6 +2,14 @@
 
 USERID=$(id -u)
 
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%F-%H-%M-%S) # TIMESTAMP=$(date +%F-%H-%M-%S)
 LOGFILE=/tmp/"$SCRIPT_NAME-$TIMESTAMP".log # LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
@@ -13,13 +21,17 @@ N="\e[0m"
 
 echo "script started executing at : $TIMESTAMP"
 
-if [ $USERID -ne 0 ]
-then
-    echo "please run this script with root access"
-    exit 1
-else
-    echo "you are super user"
-fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R failure $N"
+        exit 1
+    else
+        echo -e "$2...$G success $N"
+        
+    fi
+}
 
 # echo "all packages: $@"
 
