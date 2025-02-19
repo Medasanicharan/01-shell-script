5915189f1dd0c74b71bf97b3e1160a46cd47b99a
charan <medasanicharan66@gmail.com>
2025-02-19 08:19:31 +0530

    shell-script

--- 03-varibles.sh
+++ 03-varibles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 PERSON1=ramesh
-PERSON1=suresh
+PERSON2=suresh
 
 echo "${PERSON1}:: hello ${PERSON2}, how are you?"
 echo "${PERSON2}:: hi ${PERSON1}, i am fine. how are you"
-echo "${PERSON1}:: i am fine too. how is your work"
-echo "${PERSON2}:: not bad. i am thinking to upgrade to devops"
+echo "${PERSON1}:: i am fine too ${PERSON2}. how is your work"
+echo "${PERSON2}:: not bad ${PERSON1}. i am thinking to upgrade to devops"
