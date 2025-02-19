f7a41ca38a5567e6c9927c7c7d4c3a811e2b3176
charan <medasanicharan66@gmail.com>
2025-02-19 13:21:49 +0530

    shell-script

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+
+
 USERID=$(id -u)
-TIMESTAMP=$(echo date +%F-%H-%M-%S)
+TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
