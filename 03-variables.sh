04ff9777eeb78984d97cf2524a7c0f3315573e4c
charan <medasanicharan66@gmail.com>
2025-02-19 09:41:06 +0530

    shell-script

--- 03-variables.sh
+++ 03-variables.sh
@@ -9,3 +9,10 @@ echo "${PERSON1}:: hello ${PERSON2}, how are you?"
 echo "${PERSON2}:: hi ${PERSON1}, i am fine. how are you"
 echo "${PERSON1}:: i am fine too ${PERSON2}. how is your work"
 echo "${PERSON2}:: not bad ${PERSON1}. i am thinking to upgrade to devops"
+
+
+# # reffering variables
+# echo "$PERSON1:: hello $PERSON2, how are you?"
+# echo "$PERSON2:: hi $PERSON1, i am fine. how are you"
+# echo "$PERSON1:: i am fine too $PERSON2. how is your work"
+# echo "$PERSON2:: not bad $PERSON1. i am thinking to upgrade to devops"
---
+++ 06-array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("RRR" "munna" "murari") # list always starts wth zero
+
+echo "first movie is: ${MOVIES[0]}"
+
+echo "second movie is: ${MOVIES[1]}"
+
+echo "all movies are: ${MOVIES[@]}"
