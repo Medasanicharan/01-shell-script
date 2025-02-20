b3dfedb35abdd786f459b689e691773f6e712968
charan <medasanicharan66@gmail.com>
2025-02-20 16:01:38 +0530

    expense-project

---
+++ 17-set.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "please run this script with root access"
+    exit 1
+else
+    echo "you are super user"
+fi
+
+dnf install myddgsql -y
+
+dnf install gcc -y
