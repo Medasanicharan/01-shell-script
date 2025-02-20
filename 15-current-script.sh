634e4d50ed7409427c20b65bc3d8e0b4746b84c7
charan <medasanicharan66@gmail.com>
2025-02-20 15:14:20 +0530

    expense-project

--- 15-current-script.sh
+++ 15-current-script.sh
@@ -3,8 +3,9 @@
 COURSE="Devops from current script"
 
 echo "before calling other script, course: $COURSE"
-echo "rocess ID of current sell script: $$"
+echo "process ID of current sell script: $$"
 
-./16-other-script.sh
+# ./16-other-script.sh
+source ./16-other-script.sh
 
 echo "After calling other script, course: $COURSE"
