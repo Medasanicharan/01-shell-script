1acc5baa97eb1d460daaedfee859112bb702bab3
charan <medasanicharan66@gmail.com>
2025-02-22 12:35:51 +0530

    expense-project

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -1,3 +1,17 @@
 #!/bn/bash
 
-SOURCE_DIRECTORY=
+SOURCE_DIRECTORY=/tmp/app-logs
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+if [ -d $SOURCE_DIRECTORY ]
+then 
+    echo -e "$G Source directory exits $N"
+else 
+    echo -e "$R Please make sure $SOURCE_DIRECTORY exits $N"
+    exit 1
+fi
